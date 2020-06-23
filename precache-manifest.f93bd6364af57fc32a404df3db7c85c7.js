739785b766fc4a7577f7c27c2a25b245c8aa5f22
Ramin Roufeh <ramin1408@gmail.com>
2020-06-23 02:27:46 -0700

    Updates

---
+++ precache-manifest.f93bd6364af57fc32a404df3db7c85c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c05f45b91b6ca22a615662df2b120d",
+    "url": "/FaceRecognitionApp/index.html"
+  },
+  {
+    "revision": "a2c257901e425b8dbad7",
+    "url": "/FaceRecognitionApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e8267ca4027d85f280d0",
+    "url": "/FaceRecognitionApp/static/css/main.f4d9a048.chunk.css"
+  },
+  {
+    "revision": "a2c257901e425b8dbad7",
+    "url": "/FaceRecognitionApp/static/js/2.996a7713.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceRecognitionApp/static/js/2.996a7713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8267ca4027d85f280d0",
+    "url": "/FaceRecognitionApp/static/js/main.a3ce41ff.chunk.js"
+  },
+  {
+    "revision": "32f3d164f6a8c1b27319",
+    "url": "/FaceRecognitionApp/static/js/runtime-main.aa2b5389.js"
+  }
+]);
